be70711e7eff79c220e21f60610c6cc73cfdfbf4
GattiGabriel <ggattidev@gmail.com>
2025-05-23 00:21:28 -0300

    feat: created TaskDAO interface

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/TaskDAO.kt
@@ -0,0 +1,9 @@
+package br.edu.ifsp.scl.ads.pdm.personaltasks.model
+
+interface TaskDAO {
+    fun createTask(task: Task): Long
+    fun retrieveTask(id: Int): Task
+    fun retrieveTasks(): MutableList<Task>
+    fun updateTaskt(task: Task): Int
+    fun deleteTask(id: Int): Int
+}
