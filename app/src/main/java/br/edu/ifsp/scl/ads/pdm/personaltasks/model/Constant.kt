e4425368c708c4e30448fee5111912ae3e8fe930
GattiGabriel <ggattidev@gmail.com>
2025-05-23 05:23:57 -0300

    feat: added necessary const

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/Constant.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/Constant.kt
@@ -1,5 +1,7 @@
 package br.edu.ifsp.scl.ads.pdm.personaltasks.model
 
 object Constant {
+    const val EXTRA_TASK = "EXTRA_TASK"
+    const val EXTRA_VIEW_TASK = "EXTRA_VIEW_TASK"
     const val INVALID_TASK_ID = -1
 }
