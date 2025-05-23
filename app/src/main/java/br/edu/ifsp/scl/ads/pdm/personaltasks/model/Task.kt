941825bada3a8558664fa1e94dcdf2c7f644ae44
GattiGabriel <ggattidev@gmail.com>
2025-05-23 00:18:54 -0300

    feat: created Task class

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/Task.kt
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.ads.pdm.personaltasks.model
+
+import android.os.Parcelable
+import br.edu.ifsp.scl.ads.pdm.personaltasks.model.Constant.INVALID_TASK_ID
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+
+@Parcelize
+data class Task(
+    var id: Int? = INVALID_TASK_ID,
+    var title: String = "",
+    var description: String = "",
+    var dueDate: LocalDate? = null
+): Parcelable
