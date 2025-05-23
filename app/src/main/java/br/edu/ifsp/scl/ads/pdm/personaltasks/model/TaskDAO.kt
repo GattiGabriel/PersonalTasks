d543e0701a2f2d47cb2f87d701b74f300b5f73dc
GattiGabriel <ggattidev@gmail.com>
2025-05-23 01:25:30 -0300

    style: corrected typo

--- app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/TaskDAO.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/pdm/personaltasks/model/TaskDAO.kt
@@ -4,6 +4,6 @@ interface TaskDAO {
     fun createTask(task: Task): Long
     fun retrieveTask(id: Int): Task
     fun retrieveTasks(): MutableList<Task>
-    fun updateTaskt(task: Task): Int
+    fun updateTask(task: Task): Int
     fun deleteTask(id: Int): Int
 }
